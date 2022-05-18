acfb208b8654e805db579ef3f447a4b834d7433b
Jacob Ochoa <jacob.ochoa@gmail.com>
2022-05-18 04:02:41 -0700

    ParseBearerToken

---
+++ headers.go
@@ -0,0 +1,22 @@
+package rest
+
+import (
+	"fmt"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+var reBearToken = regexp.MustCompile("^[Bb]earer (.+)$")
+
+func ParseBearerToken(r *http.Request) (string, error) {
+	auth := r.Header.Get("authorization")
+	if !reBearToken.MatchString(auth) {
+		return "", fmt.Errorf("authorization required")
+	}
+	token := reBearToken.FindAllStringSubmatch(auth, -1)[0][1]
+	if strings.TrimSpace(token) == "" {
+		return "", fmt.Errorf("token can not be blank")
+	}
+	return token, nil
+}
